27e50d6b892ce0875787e38c4d3ac266cf81cdd5
lah1u21 <lah1u21@soton.ac.uk>
2023-04-22 13:14:44 +0200

    Wrote "maybe" code for final two problems

---
+++ Tile.hs
@@ -0,0 +1,29 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
+import TileTokens
+import TileGrammar
+import TileTypes
+import TileEval
+import System.Environment
+import Control.Exception
+import System.IO
+
+main :: IO ()
+main = catch main' noParse
+
+main' = do (fileName : _ ) <- getArgs 
+           sourceText <- readFile fileName
+           let parsedProg = parseCalc (alexScanTokens sourceText)
+           putStrLn ("\n" ++ "Parsed as " ++ show parsedProg ++ "\n")
+           putStrLn ("Type Checking : " ++ show parsedProg ++ "\n")
+           let typedProg = typeOf [] parsedProg
+           putStrLn ("Type Checking Passed with type " ++ unparseType typedProg ++ "\n")
+           let result = evalLoop parsedProg
+           putStrLn ("Evaluates to " ++ "\n" ++ unparse result ++ "\n")
+
+noParse :: ErrorCall -> IO ()
+noParse e = do let err =  show e
+               hPutStr stderr err
+               return ()
+
+
